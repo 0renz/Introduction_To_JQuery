d44a82f7d3031ca7dd6d10476d5aa27f62570548
Lorenzo <lorenzoschauenberg@gmail.com>
2023-09-24 19:13:07 -0300

    Atualização do projeto de site sobre JQuery     1 - Resolvido bug de alinhamento com scrollbar     2 - Resolvido problema com cor nos botões pós click     3 - Resolvido problema com alinhamento central caso zoom

--- introduction_to_jquery/js/script.js
+++ introduction_to_jquery/js/script.js
@@ -1,22 +1,22 @@
-function mascara(i){
-   
-    var v = i.value;
-    
-    if(isNaN(v[v.length-1])){ // impede de entrar outro caractere que não seja número
-       i.value = v.substring(0, v.length-1);
-       return;
-    }
-    
-    i.setAttribute("maxlength", "14");
-    if (v.length == 3 || v.length == 7) i.value += ".";
-    if (v.length == 11) i.value += "-";
- 
- }
+function mascara(i) {
+
+   var v = i.value;
+
+   if (isNaN(v[v.length - 1])) { // impede de entrar outro caractere que não seja número
+      i.value = v.substring(0, v.length - 1);
+      return;
+   }
+
+   i.setAttribute("maxlength", "14");
+   if (v.length == 3 || v.length == 7) i.value += ".";
+   if (v.length == 11) i.value += "-";
+
+}
 
 
- function lettersOnly(i) {
-      var regex = /[^a-z\s]/gi;
-      i.value = i.value.replace(regex, "");
- }
- 
- 
+function lettersOnly(i) {
+   var regex = /[^a-z\s]/gi;
+   i.value = i.value.replace(regex, "");
+}
+
+
